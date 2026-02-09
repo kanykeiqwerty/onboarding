4a1ec73d56238bcceb7ade815c08f29d90165934
kanykeiqwerty <ghjvgyjvgyj@gmail.com>
2026-02-09 12:46:15 +0600

    password reset done

--- account/models.py
+++ account/models.py
@@ -50,7 +50,7 @@ class DepartmentEnum(Enum):
 class CustomUser(AbstractUser):
     email = models.EmailField('email address', unique=True)
     
-    
+    activation_code = models.CharField(max_length=255, blank=True)
     username = models.CharField(max_length=255, blank=True, null=True)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
@@ -58,12 +58,14 @@ class CustomUser(AbstractUser):
         max_length=50,
         choices=[(tag.name, tag.value) for tag in PositionEnum],
         blank=True,
+        null=True,
         default=None
     )
     department = models.CharField(
         max_length=50,
         choices=[(tag.name, tag.value) for tag in DepartmentEnum],
         blank=True,
+        null=True,
         default=None
     )
     USERNAME_FIELD = 'email'
@@ -87,6 +89,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
     
+    def create_activation_code(self):
+        import uuid
+        code = str(uuid.uuid4())
+        self.activation_code = code
 
 
 # from enum import Enum
--- onboard/settings.py
+++ onboard/settings.py
@@ -67,6 +67,13 @@ MIDDLEWARE = [
 
    
 ]
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'onboard.urls'
